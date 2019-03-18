d239e8c4f3c6dade6b0fdff57826e993b9f52eb6
João Pedro Horta <joao.horta@outlook.com>
2019-03-18 18:14:32 +0000

    fix

--- CromoGest/Forms/TrocasForm.Designer.cs
+++ CromoGest/Forms/TrocasForm.Designer.cs
@@ -32,23 +32,14 @@
             this.TrocasToolTip = new System.Windows.Forms.ToolTip(this.components);
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.DataGridViewTrocas = new System.Windows.Forms.DataGridView();
-            this.Editar = new System.Windows.Forms.DataGridViewCheckBoxColumn();
-            this.IdTroca = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Progresso = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.DataProposta = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.DataEnvio = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.DataRececao = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Destinatario = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.CromosRecebidos = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.CromosEnviados = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.ButtonTrocas = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ButtonTrocas = new System.Windows.Forms.Button();
+            this.CadernetasLabel = new System.Windows.Forms.Label();
+            this.CadernetasComboBox = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DataGridViewTrocas)).BeginInit();
             this.tableLayoutPanel2.SuspendLayout();
@@ -71,124 +62,26 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(953, 610);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1148, 657);
             this.tableLayoutPanel1.TabIndex = 14;
             // 
             // DataGridViewTrocas
             // 
             this.DataGridViewTrocas.AllowUserToAddRows = false;
             this.DataGridViewTrocas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DataGridViewTrocas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Editar,
-            this.IdTroca,
-            this.Progresso,
-            this.DataProposta,
-            this.DataEnvio,
-            this.DataRececao,
-            this.Destinatario,
-            this.CromosRecebidos,
-            this.CromosEnviados});
             this.DataGridViewTrocas.Dock = System.Windows.Forms.DockStyle.Fill;
             this.DataGridViewTrocas.Location = new System.Drawing.Point(3, 43);
             this.DataGridViewTrocas.Name = "DataGridViewTrocas";
-            this.DataGridViewTrocas.Size = new System.Drawing.Size(947, 459);
+            this.DataGridViewTrocas.Size = new System.Drawing.Size(1142, 506);
             this.DataGridViewTrocas.TabIndex = 1;
-            // 
-            // Editar
-            // 
-            this.Editar.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
-            this.Editar.Frozen = true;
-            this.Editar.HeaderText = "Editar";
-            this.Editar.Name = "Editar";
-            this.Editar.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.Editar.Width = 40;
-            // 
-            // IdTroca
-            // 
-            this.IdTroca.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
-            this.IdTroca.Frozen = true;
-            this.IdTroca.HeaderText = "Nº Troca";
-            this.IdTroca.Name = "IdTroca";
-            this.IdTroca.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.IdTroca.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.IdTroca.Width = 70;
-            // 
-            // Progresso
-            // 
-            this.Progresso.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
-            this.Progresso.Frozen = true;
-            this.Progresso.HeaderText = "Progresso";
-            this.Progresso.Name = "Progresso";
-            this.Progresso.Width = 60;
-            // 
-            // DataProposta
-            // 
-            this.DataProposta.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
-            this.DataProposta.Frozen = true;
-            this.DataProposta.HeaderText = "Data Proposta";
-            this.DataProposta.Name = "DataProposta";
-            // 
-            // DataEnvio
-            // 
-            this.DataEnvio.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
-            this.DataEnvio.Frozen = true;
-            this.DataEnvio.HeaderText = "Data Envio";
-            this.DataEnvio.Name = "DataEnvio";
-            // 
-            // DataRececao
-            // 
-            this.DataRececao.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
-            this.DataRececao.Frozen = true;
-            this.DataRececao.HeaderText = "Data Receção";
-            this.DataRececao.Name = "DataRececao";
-            // 
-            // Destinatario
-            // 
-            this.Destinatario.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
-            this.Destinatario.HeaderText = "Destinatário";
-            this.Destinatario.Name = "Destinatario";
-            this.Destinatario.Width = 88;
-            // 
-            // CromosRecebidos
-            // 
-            this.CromosRecebidos.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.CromosRecebidos.HeaderText = "Cromos Recebidos";
-            this.CromosRecebidos.Name = "CromosRecebidos";
-            this.CromosRecebidos.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.CromosRecebidos.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            // 
-            // CromosEnviados
-            // 
-            this.CromosEnviados.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.CromosEnviados.HeaderText = "Cromos Enviados";
-            this.CromosEnviados.Name = "CromosEnviados";
             // 
             // statusStrip1
             // 
-            this.statusStrip1.Location = new System.Drawing.Point(0, 588);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 635);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(953, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(1148, 22);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(443, 6);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(281, 21);
-            this.comboBox1.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(402, 10);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
             // 
             // tableLayoutPanel2
             // 
@@ -203,49 +96,16 @@
             this.tableLayoutPanel2.Controls.Add(this.button3, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.ButtonTrocas, 0, 0);
-            this.tableLayoutPanel2.Controls.Add(this.label1, 3, 0);
-            this.tableLayoutPanel2.Controls.Add(this.comboBox1, 4, 0);
+            this.tableLayoutPanel2.Controls.Add(this.CadernetasLabel, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.CadernetasComboBox, 4, 0);
             this.tableLayoutPanel2.Controls.Add(this.button2, 6, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(947, 34);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1142, 34);
             this.tableLayoutPanel2.TabIndex = 3;
-            // 
-            // ButtonTrocas
-            // 
-            this.ButtonTrocas.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.ButtonTrocas.ForeColor = System.Drawing.Color.Black;
-            this.ButtonTrocas.Location = new System.Drawing.Point(3, 5);
-            this.ButtonTrocas.Name = "ButtonTrocas";
-            this.ButtonTrocas.Size = new System.Drawing.Size(104, 23);
-            this.ButtonTrocas.TabIndex = 12;
-            this.ButtonTrocas.Text = "&Trocas";
-            this.ButtonTrocas.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(113, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(104, 23);
-            this.button1.TabIndex = 13;
-            this.button1.Text = "&Trocas";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.button2.ForeColor = System.Drawing.Color.Black;
-            this.button2.Location = new System.Drawing.Point(840, 5);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(104, 23);
-            this.button2.TabIndex = 14;
-            this.button2.Text = "&Trocas";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
@@ -258,12 +118,64 @@
             this.button3.Text = "&Trocas";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(113, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "&Trocas";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ButtonTrocas
+            // 
+            this.ButtonTrocas.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.ButtonTrocas.ForeColor = System.Drawing.Color.Black;
+            this.ButtonTrocas.Location = new System.Drawing.Point(3, 5);
+            this.ButtonTrocas.Name = "ButtonTrocas";
+            this.ButtonTrocas.Size = new System.Drawing.Size(104, 23);
+            this.ButtonTrocas.TabIndex = 12;
+            this.ButtonTrocas.Text = "&Trocas";
+            this.ButtonTrocas.UseVisualStyleBackColor = true;
+            // 
+            // CadernetasLabel
+            // 
+            this.CadernetasLabel.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.CadernetasLabel.AutoSize = true;
+            this.CadernetasLabel.Location = new System.Drawing.Point(376, 10);
+            this.CadernetasLabel.Name = "CadernetasLabel";
+            this.CadernetasLabel.Size = new System.Drawing.Size(61, 13);
+            this.CadernetasLabel.TabIndex = 0;
+            this.CadernetasLabel.Text = "Cadernetas";
+            // 
+            // CadernetasComboBox
+            // 
+            this.CadernetasComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.CadernetasComboBox.FormattingEnabled = true;
+            this.CadernetasComboBox.Location = new System.Drawing.Point(443, 6);
+            this.CadernetasComboBox.Name = "CadernetasComboBox";
+            this.CadernetasComboBox.Size = new System.Drawing.Size(476, 21);
+            this.CadernetasComboBox.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(1035, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "&Trocas";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // TrocasForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(953, 610);
+            this.ClientSize = new System.Drawing.Size(1148, 657);
             this.Controls.Add(this.tableLayoutPanel1);
             this.ForeColor = System.Drawing.Color.White;
             this.MinimumSize = new System.Drawing.Size(860, 468);
@@ -282,19 +194,10 @@
         private System.Windows.Forms.ToolTip TrocasToolTip;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.DataGridView DataGridViewTrocas;
-        private System.Windows.Forms.DataGridViewCheckBoxColumn Editar;
-        private System.Windows.Forms.DataGridViewTextBoxColumn IdTroca;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Progresso;
-        private System.Windows.Forms.DataGridViewTextBoxColumn DataProposta;
-        private System.Windows.Forms.DataGridViewTextBoxColumn DataEnvio;
-        private System.Windows.Forms.DataGridViewTextBoxColumn DataRececao;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Destinatario;
-        private System.Windows.Forms.DataGridViewTextBoxColumn CromosRecebidos;
-        private System.Windows.Forms.DataGridViewTextBoxColumn CromosEnviados;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label CadernetasLabel;
+        private System.Windows.Forms.ComboBox CadernetasComboBox;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button ButtonTrocas;
