f846e7df0941a9e78e130adc36916595381b0a0e
João Pedro Horta <joao.horta@outlook.com>
2019-04-02 11:26:48 +0100

    Revert "bug fix"
    This reverts commit ca7219ed85c42d2c4c8f686e227d7e22a7c9411b.

--- CromoGest/Forms/EntradasForm.cs
+++ CromoGest/Forms/EntradasForm.cs
@@ -77,6 +77,7 @@ namespace CromoGest
         {
             if (!CromosValidos()) return;
             bool IsNovo;
+            int oldQuant;
             int rows = dataGridViewCromos.Rows.Count - 1;
 
             LimpaResultados();
@@ -88,6 +89,7 @@ namespace CromoGest
                     string[] cromos = row.Replace(" ", string.Empty).Split(charSeparador);
                     foreach (string cromo in cromos)
                     {
+                        oldQuant = GetCromoQuantidade(cromo);
                         IsNovo = (IncCromoQuantidade(cromo)==1);
                         AddCromoToResults(cromo, IsNovo);
                     }
@@ -95,6 +97,7 @@ namespace CromoGest
                 else
                 {
                     string cromo = row;
+                    oldQuant = GetCromoQuantidade(cromo);
                     IsNovo = (IncCromoQuantidade(cromo) == 1);
                     AddCromoToResults(cromo, IsNovo);
                 }
