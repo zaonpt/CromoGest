0aa4f82e7da2c5436943ee88cf96c26e0d4b1903
João Pedro Horta <joao.horta@outlook.com>
2019-04-22 13:38:13 +0100

    fixes na class trocas

--- CromoGestLibrary/Modelos/TrocaModelo.cs
+++ CromoGestLibrary/Modelos/TrocaModelo.cs
@@ -10,94 +10,112 @@ namespace CromoGestLibrary.Modelos
     public class TrocaModelo : INotifyPropertyChanged
     {
 
-        private char charSeparador = CromoGestLibrary.GlobalConfig.Connection.GetConfig(ConfigCategory.CharSeparador.Value)[0];
+        private readonly char charSeparador = CromoGestLibrary.GlobalConfig.Connection.GetConfig(ConfigCategory.CharSeparador.Value)[0];
 
         /// <summary>
         /// Id da base de dados
         /// </summary>
         public int Id { get; set; }
 
+
         /// <summary>
         /// Numero da Troca usado pelo interface
         /// </summary>
+        private int _numTroca;
         public int NumTroca
         {
-            get { return NumTroca; }
+            get { return _numTroca; }
             set
             {
-                if (NumTroca != value)
+                if (_numTroca != value)
                 {
-                    NumTroca = value;
+                    _numTroca = value;
                     OnPropertyChanged("NumTroca");
                 }
             }
         }
-
-        /// <summary>
-        /// Destinatário da troca
-        /// </summary>
-       // public DestinatarioModelo Destinatario { get; set; } = new DestinatarioModelo();
-
+        
+       /// <summary>
+       /// Destinatário da troca
+       /// </summary>
         public int Dest_Id { get; set; }
+        private string _dest_Nome;
         public string Dest_Nome
         {
-            get { return Dest_Nome; }
+            get { return _dest_Nome; }
             set
             {
-                if (Dest_Nome != value)
+                if (_dest_Nome != value)
                 {
-                    Dest_Nome = value;
+                    _dest_Nome = value;
                     OnPropertyChanged("Dest_Nome");
                 }
             }
         }
+
+        /// <summary>
+        /// Iniciais do Destinatário
+        /// </summary>
+        private string _dest_Iniciais;
         public string Dest_Iniciais
         {
-            get { return Dest_Iniciais; }
+            get { return _dest_Iniciais; }
             set
             {
-                if (Dest_Iniciais != value)
+                if (_dest_Iniciais != value)
                 {
-                    Dest_Iniciais = value;
+                    _dest_Iniciais = value;
                     OnPropertyChanged("Dest_Iniciais");
                 }
             }
         }
 
+        /// <summary>
+        /// Origem do Destinatário
+        /// </summary>
+        private string _dest_Origem;
         public string Dest_Origem
         {
-            get { return Dest_Origem; }
+            get { return _dest_Origem; }
             set
             {
-                if (Dest_Origem != value)
+                if (_dest_Origem != value)
                 {
-                    Dest_Origem = value;
+                    _dest_Origem = value;
                     OnPropertyChanged("Dest_Origem");
                 }
             }
         }
 
+        /// <summary>
+        /// Reputação do destinatário
+        /// </summary>
+        private string _dest_reputacao;
         public string Dest_Reputacao
         {
-            get { return Dest_Reputacao; }
+            get { return _dest_reputacao; }
             set
             {
-                if (Dest_Reputacao != value)
+                if (_dest_reputacao != value)
                 {
-                    Dest_Reputacao = value;
+                    _dest_reputacao = value;
                     OnPropertyChanged("Dest_Reputacao");
                 }
             }
         }
 
+        /// <summary>
+        /// Morada do Destinatário
+        /// </summary>
+        private string _dest_Morada;
         public string Dest_Morada
         {
-            get { return Dest_Morada; }
+            get { return _dest_Morada; }
             set
             {
-                if (Dest_Morada != value)
+                if (_dest_Morada != value)
                 {
-                    Dest_Morada = value;
+                    _dest_Morada = value;
                     OnPropertyChanged("Dest_Morada");
                 }
             }
@@ -106,14 +124,15 @@ namespace CromoGestLibrary.Modelos
         /// <summary>
         /// Progresso da troca (Altenativas recolhidas da BD)
         /// </summary>
+        private string _progresso;
         public string Progresso
         {
-            get { return Progresso; }
+            get { return _progresso; }
             set
             {
-                if (Progresso != value)
+                if (_progresso != value)
                 {
-                    Progresso = value;
+                    _progresso = value;
                     OnPropertyChanged("Progresso");
                 }
             }
@@ -122,14 +141,15 @@ namespace CromoGestLibrary.Modelos
         /// <summary>
         /// Data em que foi proposta a troca
         /// </summary>
+        private DateTime _dataProposta;
         public DateTime DataProposta
         {
-            get { return DataProposta; }
+            get { return _dataProposta; }
             set
             {
-                if (DataProposta != value)
+                if (_dataProposta != value)
                 {
-                    DataProposta = value;
+                    _dataProposta = value;
                     OnPropertyChanged("DataProposta");
                 }
             }
@@ -138,14 +158,15 @@ namespace CromoGestLibrary.Modelos
         /// <summary>
         /// Data em que foram enviados os cromos desta troca
         /// </summary>
+        private DateTime _dataEnvio;
         public DateTime DataEnvio
         {
-            get { return DataEnvio; }
+            get { return _dataEnvio; }
             set
             {
-                if (DataEnvio != value)
+                if (_dataEnvio != value)
                 {
-                    DataEnvio = value;
+                    _dataEnvio = value;
                     OnPropertyChanged("DataEnvio");
                 }
             }
@@ -154,14 +175,15 @@ namespace CromoGestLibrary.Modelos
         /// <summary>
         /// Data de receção dos cromos desta troca
         /// </summary>
+        private DateTime _dataRececao;
         public DateTime DataRececao
         {
-            get { return DataRececao; }
+            get { return _dataRececao; }
             set
             {
-                if (DataRececao != value)
+                if (_dataRececao != value)
                 {
-                    DataRececao = value;
+                    _dataRececao = value;
                     OnPropertyChanged("DataRececao");
                 }
             }
